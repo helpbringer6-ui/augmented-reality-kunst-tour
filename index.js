4f9a1a9fd0b47398893105ca4c1e95104ee6990b
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-15 07:35:18 -0500

    Add comments to clarify code purpose

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// Importiere die benötigten Module von React Native
 import { AppRegistry } from 'react-native';
+// Importiere die Haupt-App-Komponente
 import App from './src/App';
+// Importiere den Anwendungsnamen aus der Konfigurationsdatei
 import { name as appName } from './app.json';
 
+// Registriere die Hauptkomponente der App
 AppRegistry.registerComponent(appName, () => App);
