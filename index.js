044deb834a1603f3d9fa494247ceb654040bf9f3
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-15 07:49:38 -0500

    Add comments explaining the import statements

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Importiere die benötigten Module von React Native
-import { AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native'; // Ermöglicht die Registrierung von Komponenten
 // Importiere die Haupt-App-Komponente
-import App from './src/App';
+import App from './src/App'; // Importiert die Hauptanwendungslogik und UI
 // Importiere den Anwendungsnamen aus der Konfigurationsdatei
-import { name as appName } from './app.json';
+import { name as appName } from './app.json'; // Lädt den Namen der Anwendung aus der Konfiguration
 
 // Registriere die Hauptkomponente der App unter dem angegebenen Namen
 // Dies ist der Einstiegspunkt der Anwendung.
